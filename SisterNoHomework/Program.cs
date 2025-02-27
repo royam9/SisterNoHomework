0510b896bb7a6f2c637f259aa1db656e6202472e
royam9 <lolm19070@gmail.com>
2025-02-27 22:21:09 +0800

    加入專案檔案。

---
+++ SisterNoHomework/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace SisterNoHomework;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("請輸入頭數量");
+        int head = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("請輸入腳數量");
+        int feet = Convert.ToInt32(Console.ReadLine());
+
+        if (head < 0 || feet < 0)
+        {
+            Console.WriteLine("請輸入正數"); 
+            return;
+        }
+
+        if (feet % 2 != 0)
+        {
+            Console.WriteLine("請不要把有殘缺腳的動物關在籠子裡");
+            return;
+        }
+
+        // 2x + 4y = feet
+        // x + 2y = feet / 2
+        // x + y = head
+        // feet / 2  - head = y 
+
+        int rabbitCount = feet / 2  - head;
+
+        if (rabbitCount < 0)
+        {
+            Console.WriteLine("頭太多了齁");
+            return;
+        }
+
+        int chickenCount = head - rabbitCount;
+
+        Console.WriteLine($"兔子數量{rabbitCount}，雞數量{chickenCount}");
+    }
+}
