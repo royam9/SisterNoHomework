9a55c9b060ec4cff2e6ecdde19299c22019f2c18
royam9 <lolm19070@gmail.com>
2025-02-27 22:37:03 +0800

    Exception catch

--- SisterNoHomework/Program.cs
+++ SisterNoHomework/Program.cs
@@ -5,20 +5,31 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("請輸入頭數量");
-        int head = Convert.ToInt32(Console.ReadLine());
+
+        if (!int.TryParse(Console.ReadLine(), out int head))
+        {
+            Console.WriteLine("請輸入正確參數");
+            ReStart();
+        };    
+        
         Console.WriteLine("請輸入腳數量");
-        int feet = Convert.ToInt32(Console.ReadLine());
+
+        if (!int.TryParse(Console.ReadLine(), out int feet))
+        {
+            Console.WriteLine("請輸入正確參數");
+            ReStart();
+        };
 
         if (head < 0 || feet < 0)
         {
-            Console.WriteLine("請輸入正數"); 
-            return;
+            Console.WriteLine("請輸入正數");
+            ReStart();
         }
 
         if (feet % 2 != 0)
         {
             Console.WriteLine("請不要把有殘缺腳的動物關在籠子裡");
-            return;
+            ReStart();
         }
 
         // 2x + 4y = feet
@@ -26,16 +37,30 @@ internal class Program
         // x + y = head
         // feet / 2  - head = y 
 
-        int rabbitCount = feet / 2  - head;
+        int rabbitCount = feet / 2 - head;
 
         if (rabbitCount < 0)
         {
             Console.WriteLine("頭太多了齁");
-            return;
+            ReStart();
         }
 
         int chickenCount = head - rabbitCount;
 
-        Console.WriteLine($"兔子數量{rabbitCount}，雞數量{chickenCount}");
+        if (chickenCount < 0)
+        {
+            Console.WriteLine("輸入參數錯誤了啦");
+            ReStart();
+        }
+
+        Console.WriteLine($"兔子數量：{rabbitCount}，雞數量：{chickenCount}");
+        ReStart();
+    }
+
+    private static void ReStart()
+    {
+        AppDomain.CurrentDomain.ExecuteAssembly(
+        System.Reflection.Assembly.GetExecutingAssembly().Location
+        );
     }
 }
